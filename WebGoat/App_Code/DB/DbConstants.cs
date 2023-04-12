18f92785372e11b196506010fb493b529da447e7
Rommel Sunga <lightingthefuse@gmail.com>
2023-04-12 13:44:40 +0800

    Update DbConstants.cs

--- WebGoat/App_Code/DB/DbConstants.cs
+++ WebGoat/App_Code/DB/DbConstants.cs
@@ -13,7 +13,7 @@ namespace OWASP.WebGoat.NET.App_Code.DB
         public const string KEY_FILE_NAME = "filename";
         public const string KEY_DATABASE = "database";
         public const string KEY_UID = "uid";
-        public const string KEY_PWD = "pwd";
+        //public const string KEY_PWD = "pwd";
             
         //DB Types
         public const string DB_TYPE_MYSQL = "MySql";
